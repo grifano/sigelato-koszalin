629a4e384946ddd93c9457ff69f814808457e662
Serhii Orlenko <146512960+grifano@users.noreply.github.com>
2025-05-04 14:01:35 +0200

    create latest nextjs project

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+const Home = () => {
+  return (
+    <>
+      <h1 className="font-heading">
+        Welcome to <span className="font-heading-em">Sigelato</span>
+      </h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero, odit.
+      </p>
+    </>
+  );
+};
+
+export default Home;
