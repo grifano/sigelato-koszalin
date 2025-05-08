772812a0074f695b543a975b293ba4746b43fa47
Serhii Orlenko <146512960+grifano@users.noreply.github.com>
2025-05-08 11:01:10 +0200

    remove paragraph from home page

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,6 @@ const Home = () => {
       <h1 className="font-heading">
         Welcome to <span className="font-heading-em">Sigelato</span>
       </h1>
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero, odit.
-      </p>
     </>
   );
 };
